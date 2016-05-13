489e9634e8defb8f62b831a8b079f850ba094635
HWP <HWP@hwp.com>
2016-05-14 00:53:09 +0800

    commit icon launchImage

---
+++ WPTabBarVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WPTabBarVC
+//
+//  Created by huangweipeng on 16/5/14.
+//  Copyright © 2016年 HWP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
